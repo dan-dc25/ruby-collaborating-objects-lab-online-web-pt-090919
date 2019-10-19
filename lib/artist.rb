4ce10104262f8f7b359ee029e8df0616b3c02186
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-10-19 03:13:11 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,7 +21,11 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    self.find(name)
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
   end
 
 
